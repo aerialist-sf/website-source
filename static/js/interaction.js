9eb1a447d6ae1135ea3b0886631128de13d99d2e
malcolm <malcolm@hoprocker.net>
2014-07-06 19:52:34 -0700

    adding dialog

---
+++ static/js/interaction.js
@@ -0,0 +1,27 @@
+$(function() {
+  /*
+  $(document).tooltip({
+    items:    "#book-now",
+    content:  "<div class='ui-tooltip'>Click here to find out more!</div>",
+    hide:     false,
+    position: {my: "center center", at: "right center"},
+    show:     100
+  });
+  */
+  $("#book-dialog").dialog({
+    autoOpen: false,
+    draggable: false,
+    height: "auto",
+    hide: "fadeOut",
+    modal: true,
+    position: {my: "top", at: "top+10%", of: window},
+    resizable: false,
+    show: "slideDown",
+    width: 600
+    });
+  $("#book-now a:first").on("click", function() {
+    $("#book-dialog").dialog("open");
+  });
+});
+
+
