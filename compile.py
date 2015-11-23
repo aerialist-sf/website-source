de68aac674c59c3d9673524e5857378e49ceca3b
malcolm <malcolm@hoprocker.net>
2015-11-22 19:17:08 -0800

    setup multi-page photos

--- compile.py
+++ compile.py
@@ -8,11 +8,16 @@ from jinja2 import Template, FileSystemLoader, Environment
 
 TMPL_PATH = "templates/"
 STATIC_PATH = "static/"
-SLIDES_PATH = "slides/"
-CAPTIONS_PATH = os.path.join(SLIDES_PATH, "_ATTRIBUTIONS.txt")
+SLIDES_PATHS = [{'page': 'photos_%s' % name,
+                 'folder': 'slides/%s/' % name}
+                    for name in ['vaudevire', 'rjmuna', 'other']]
+                
+
+CAPTIONS_FILE = "_ATTRIBUTIONS.txt"
 OUTPUT = "site/"
 
-PAGES = ["index", "photos", "video", "bio", "booking"]
+PAGES = ["index", "gallery", "video", "bio", "booking", "photos_rjmuna", "photos_vaudevire", "photos_other"]
+NAV_PAGES = ["index", "gallery", "video", "bio", "booking"]
 
 def setup():
     shutil.rmtree(os.path.join(OUTPUT, STATIC_PATH))
@@ -50,7 +55,8 @@ def compile_templates():
     pages = get_valid_templates()
     env_vars = {"nav_pages":[{"url":"/%s/" % t if t != "index" else "/",
                             "name":t.title() if t != "index" else "Home",
-                            "id":t} for t in pages]}
+                            "id":t} for t in pages
+                                    if t in NAV_PAGES]}
     for tmpl in pages:
         print "working on %s" % tmpl
         tmpl_compile(tmpl, env_vars)
@@ -63,22 +69,26 @@ def slides():
     """
     Assemble the slides.
     """
-    print "copying slides"
-    slides_paths = []
-    try:
-        os.mkdir(os.path.join(OUTPUT, STATIC_PATH, SLIDES_PATH))
-    except OSError:
-        ## dir exists, pass
-        pass
-    
-    with closing(open(CAPTIONS_PATH, "r")) as f:
-        captions = json.loads(f.read())['captions']
-    for s in [l for l in os.listdir(SLIDES_PATH) if re.match("^.*\.(jpg|jpeg)$", l)]:
-        output_path = os.path.join(STATIC_PATH, SLIDES_PATH, s)
-        shutil.copyfile(os.path.join(SLIDES_PATH, s), os.path.join(OUTPUT, output_path))
-        title = captions[s] if s in captions.keys() else s
-        slides_paths.append({"title":title, "href":"/%s" % output_path})
-    return slides_paths
+    all_slides = {}
+    for slide_info in SLIDES_PATHS:
+        print "copying %(page)s slides" % slide_info
+        slides_paths = []
+        try:
+            os.makedirs(os.path.join(OUTPUT, STATIC_PATH, slide_info['folder']))
+        except OSError, e:
+            ## dir exists
+            pass
+
+        captions_path = os.path.join(slide_info['folder'], CAPTIONS_FILE)
+        with closing(open(captions_path, "r")) as f:
+            captions = json.loads(f.read())['captions']
+        for s in [l for l in os.listdir(slide_info['folder']) if re.match("^.*\.(jpg|jpeg|png)$", l)]:
+            output_path = os.path.join(STATIC_PATH, slide_info['folder'], s)
+            shutil.copyfile(os.path.join(slide_info['folder'], s), os.path.join(OUTPUT, output_path))
+            title = captions[s] if s in captions.keys() else s
+            slides_paths.append({"title":title, "href":"/%s" % output_path})
+        all_slides.update({slide_info['page']: slides_paths})
+    return all_slides
 
 if __name__ == "__main__":
     run_compilation()
