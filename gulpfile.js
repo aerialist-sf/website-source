f8032d67d40a4e5c73b0199d74bb78bec06eccb6
malcolm <malcolm@hoprocker.net>
2015-11-22 18:37:21 -0800

    add Gulp for managing compilation

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+//var browserSync = require('browser-sync').browserSync;
+var changed = require('gulp-changed');
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
+//var flatten = require('gulp-flatten');
+var runSequence = require('run-sequence');
+
+gulp.task('static', function() {
+  return gulp.src('static/**')
+    .on('error', function (err) {
+      console.log(err);
+      gutil.beep();
+      this.end();
+    })
+    .pipe(gulp.dest('site/static'));
+});
+
+gulp.task('templates', function() {
+  exec('./env/bin/python compile.py', function(err, stdout, stderr) {
+    console.log(stdout);
+    if(err || stderr) {
+      console.log(stderr);
+      console.log(err);
+      gutil.beep();
+    }
+  });
+});
+
+gulp.task('build', function() {
+  return runSequence('templates');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('*.py', ['templates']);
+  gulp.watch('templates/**/*.html', ['templates']);
+  gulp.watch('static/**/*.{js,jpg,jpeg,png}', ['static']);
+  gulp.watch('static/**/*.css', ['static']);
+});
